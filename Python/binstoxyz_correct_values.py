4f51b64debd2930bcbb479c2691b143fc2e4a973
Jeroen van Baarle <jeroenvanbaarle@Jeroens-MacBook-Pro.local>
2018-01-12 13:04:36 +0100

    kmeans complete, crossplatform path in binstoxyz

--- Python/binstoxyz_correct_values.py
+++ Python/binstoxyz_correct_values.py
@@ -2,12 +2,20 @@ import h5py
 import numpy as np
 import math
 import csv
+import sys
 
+# Get current OS to avoid errors in path
 
-datasets = h5py.File(r'C:\Users\Emmaa\Downloads\160930\30\03\30\merged\nldbl_pvol_20160930T0330Z.h5','r')
+os = sys.platform
 
-""" add_heights voegt de data van radarbeelden van verschillende hoogten samen tot een lijst met dictonaries 
-    met DBHZ waarden en coordinaten."""  
+if(os == "windows"):
+    datasets = h5py.File(r'C:\Users\Emmaa\Downloads\160930\30\03\30\merged\nldbl_pvol_20160930T0330Z.h5','r')
+else:
+    datasets = h5py.File('RadarData/160930/20/30/merged/nldbl_pvol_20160930T2030Z.h5', 'r')
+
+# add_heights voegt de data van radarbeelden van verschillende hoogten samen tot een lijst met dictonaries
+# met DBHZ waarden en coordinaten.
+
 def add_heights(datasets):
     points = []
     for data in datasets:
@@ -24,8 +32,8 @@ def add_heights(datasets):
             points.extend(get_coordinates_picture(nbins, nrays, elevation_angle, bin_distance, data_DBZH, data_TH, data_VRAD, offsets, gains))
     return points
 
-""" get_coordinates_picture berekend van elk punt in een radarafbeelding zijn x,y en z coordinaat
-    deze worden opgeslagen in een lijst van dictionaries samen met de DBZH waarde van elk punt."""    
+# get_coordinates_picture berekend van elk punt in een radarafbeelding zijn x,y en z coordinaat
+# deze worden opgeslagen in een lijst van dictionaries samen met de DBZH waarde van elk punt.
 def get_coordinates_picture(nbins, nrays, elevation_angle, bin_distance, data_DBZH, data_TH, data_VRAD, offsets, gains):
     pic = []
     for bin_number in range(nbins):
@@ -37,15 +45,15 @@ def get_coordinates_picture(nbins, nrays, elevation_angle, bin_distance, data_DB
     return pic
             
 
-"""# get_xyz berekend de x, y en z coordinaat van een radarbin.
-    INPUT:
-    elevation angle: de hoek van de radarbeam t.o.v. de grond
-    bin_number: de n-de radarbin van een beam
-    bin_distance: de afstand tussen de radarbins
-    angle_number: de n-de beam van de radar in de x-y richting
-    total_angles: het totaal aantal beams wat is genomen in een rondje
-    OUTPUT:
-    de x, y, en z coordinaat van de radabin"""    
+#    get_xyz berekend de x, y en z coordinaat van een radarbin.
+#    INPUT:
+#    elevation angle: de hoek van de radarbeam t.o.v. de grond
+#    bin_number: de n-de radarbin van een beam
+#    bin_distance: de afstand tussen de radarbins
+#    angle_number: de n-de beam van de radar in de x-y richting
+#    total_angles: het totaal aantal beams wat is genomen in een rondje
+#    OUTPUT:
+#    de x, y, en z coordinaat van de radabin
 def get_xyz(elevation_angle, bin_number, bin_distance, angle_number, total_angles):
     z = bin_number * bin_distance * math.sin(math.radians(elevation_angle))
     dis = bin_number * bin_distance * math.cos(math.radians(elevation_angle))
@@ -56,16 +64,27 @@ def get_xyz(elevation_angle, bin_number, bin_distance, angle_number, total_angle
     
 points = add_heights(datasets) 
 
-with open(r'C:\Users\Emmaa\Documents\Studie\KI\pampam1.csv', 'w') as csvfile:
-    fieldnames = ['DBZH', 'TH', 'VRAD','x', 'y', 'z']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-    writer.writeheader()
-    # skip points where DBHZ is 0
-    for point in points:
-        if point["DBZH"] > 0.001:
-            writer.writerow(point)
+if(os == "windows"):
+    with open(r'testData\complete.csv', 'w') as csvfile:
+        fieldnames = ['DBZH', 'TH', 'VRAD','x', 'y', 'z']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
+        writer.writeheader()
+        # skip points where DBHZ is 0
+        for point in points:
+            if point["DBZH"] > 0.001:
+                writer.writerow(point)
 
+else:
+    with open('testData/complete.csv', 'w') as csvfile:
+        fieldnames = ['DBZH', 'TH', 'VRAD', 'x', 'y', 'z']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        # skip points where DBHZ is 0
+        for point in points:
+            if point["DBZH"] > 0.001:
+                writer.writerow(point)
     
 
