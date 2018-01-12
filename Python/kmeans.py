11f3e6d7ede6c25b07f1360f538581e014c3bd88
JonasHans <jonas.vanoenen@gmail.com>
2018-01-12 16:38:53 +0100

    first k means tests

--- Python/kmeans.py
+++ Python/kmeans.py
@@ -6,21 +6,23 @@ import shutil as su
 def kmeans(path_and_name):
     original = np.genfromtxt(path_and_name, delimiter=',')
     headless = original[1:]
-    data = headless[:, 0]
-    data = data.reshape(-1,1)
-    print(data)
-    ks = KMeans(n_clusters=3).fit(data)
 
-    print("Labels for kmeans: ")
+    DBZ_weight = 100
+    clusters = 5
+    dataDBZ = headless[:, 0]*DBZ_weight
+    dataZ = headless[:, 5]
+
+    data = np.matrix(zip(dataDBZ, dataZ))
+
+    ks = KMeans(n_clusters=clusters).fit(data)
     labels = ks.labels_
-    #labels = np.append("cluster", labels)
-    print(labels)
 
     newdata = np.append(np.reshape(headless[:,3], (-1,1)), np.reshape(headless[:,4], (-1,1)), axis=1)
     newdata = np.append(newdata, np.reshape(headless[:,5], (-1,1)), axis=1)
     newdata = np.append(newdata, np.reshape(labels,(-1,1)), axis=1)
 
-    np.savetxt("testdata/new.csv", newdata, delimiter=',')
+    filename = "DBZ="+str(DBZ_weight)+" and clusters="+str(clusters)
+    np.savetxt("output/"+filename+".csv", newdata, delimiter=',')
+    print "Created csv file: "+filename
 
-
-kmeans("testdata/complete.csv")
+kmeans("csvData/160930/160930-07-00.csv")
