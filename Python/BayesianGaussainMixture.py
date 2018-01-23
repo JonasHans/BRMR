ba96326852e4ddd77778e8dab177065e1706e44b
Jeroen van Baarle <jeroenvanbaarle@wcw-stud-145-109-35-90.wireless.uva.nl>
2018-01-23 10:20:23 +0100

    BGM

--- Python/BayesianGaussainMixture.py
+++ Python/BayesianGaussainMixture.py
@@ -18,7 +18,7 @@ def bgm(path_and_name):
     trainset = data[:div]
     testset = data[div:]
 
-    mix = BayesianGaussianMixture(n_components=3, weight_concentration_prior=0.001).fit(trainset)
+    mix = BayesianGaussianMixture(n_components=8, weight_concentration_prior=0.00001).fit(trainset)
 
     labels = mix.predict(testset)
     print labels
